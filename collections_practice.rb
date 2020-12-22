ba85d4ca7395827f38308974bcc72a102c3f8b83
JC <bonassin@gmail.com>
2020-12-22 21:28:16 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -38,3 +38,13 @@ end
 def sum_array(array)
   array.inject(:+)
 end
+
+def add_s(array)
+  array.each_with_index.collect do |string, index|
+    if index == 1
+      string
+    else
+      string << "s"
+    end
+  end
+end
